e3d97796b2b51571a6789c5271d6e932bc3f8d4a
Mikel984 <rocamiguel84@gmail.com>
2023-01-11 22:07:20 +0100

    Commenting declaration of getComputerChoice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,10 +2,14 @@
     de forma aleatoria */
 var computerOptions = ["rock", "paper", "scissors"];
 
+/* 2. Creamos funcion getComputerChoice usando Math.random convinado con Math.floor
+      para determinar la posicion del elemento de nuestra array que retorna como valor
+      para que sea aleatorio */
+      
 function getComputerChoice(){
     return computerOptions[Math.floor(Math.random()*computerOptions.length)]
 }
-
+console.log(getComputerChoice());
 
 
 
